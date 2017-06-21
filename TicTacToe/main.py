a27318fa19604c7c7ae01992a3037fcae6819001
simonkrol <simonkrol11@gmail.com>
2017-06-20 23:40:03 -0400

    Add checkwin and playthrough

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -6,11 +6,51 @@ def draw_board(board):
 			print("|"+j, end="")
 		print("|")
 	print("-------")
-player_token=None
-while(player_token!="X" and player_token!="O"):
-	player_token=input("Please enter your token(X or O)").upper()
-curr_turn=None
-while(curr_turn!="X" and curr_turn!="O"):
-	curr_turn=input("Which token should play first?").upper()
-board=[["X" for k in range(3)]]*3
+def get_not_valid(message):
+	returnable=None
+	while(returnable!="X" and returnable!="O"):
+		returnable=input(message).upper()
+	return returnable
+def set_piece(board, player_val):
+	row=int(input("Row:"))
+	column=int(input("Column:"))
+	while(board[row-1][column-1]!=" "):
+		row=int(input("Row:"))
+		column=int(input("Column:"))
+	board[row-1][column-1]=player_val
+	return board
+def switch_player(player):
+	if(player=="X"):
+		return "O"
+	else:
+		return "X"
+def turn(board, player_val):
+	set_piece(board, player_val)
+	win=check_winner(board, player_val)
+	draw_board(board)
+	return win
+def check_winner(bo, le):
+ # Given a board and a player’s letter, this function returns True if that player has won.
+ # We use bo instead of board and le instead of letter so we don’t have to type as much.
+	return ((bo[2][0] == le and bo[2][1] == le and bo[2][2] == le) or # across the top
+	(bo[1][0] == le and bo[1][1] == le and bo[1][2] == le) or # across the middle
+	(bo[0][0] == le and bo[0][1] == le and bo[0][2] == le) or # across the bottom
+	(bo[2][0] == le and bo[1][0] == le and bo[0][0] == le) or # down the left side
+	(bo[2][1] == le and bo[1][1] == le and bo[0][1] == le) or # down the middle
+	(bo[2][2] == le and bo[1][2] == le and bo[0][2] == le) or # down the right side
+	(bo[2][0] == le and bo[1][1]== le and bo[0][2] == le) or # diagonal
+	(bo[2][2] == le and bo[1][1] == le and bo[0][0] == le)) # diagonal
+player_token=get_not_valid("Please enter your token(X or O)")
+curr_turn=get_not_valid("Which token should play first?")
+board=[[" " for x in range(3)] for y in range(3)] 
+print(board)
 draw_board(board)
+set_piece(board, curr_turn)
+for turn_num in range(9):
+	result=turn(board, curr_turn)
+	if(result):
+		print(curr_turn +" has won!")
+		break;
+	if(turn_num==8):
+		print("Its a draw!")
+	curr_turn=switch_player(curr_turn)
