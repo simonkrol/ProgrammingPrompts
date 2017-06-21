40bee70dccede3cfe80e02fef235a26b2f55e843
simonkrol <simonkrol11@gmail.com>
2017-06-20 22:50:05 -0400

    Add board printing

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,1 +1,16 @@
 #https://www.reddit.com/r/ProgrammingPrompts/comments/3mg2im/easymedium_tictactoe/
+def draw_board(board):
+	print("-------")
+	for i in board:	
+		for j in i:
+			print("|"+j, end="")
+		print("|")
+	print("-------")
+player_token=None
+while(player_token!="X" and player_token!="O"):
+	player_token=input("Please enter your token(X or O)").upper()
+curr_turn=None
+while(curr_turn!="X" and curr_turn!="O"):
+	curr_turn=input("Which token should play first?").upper()
+board=[["X" for k in range(3)]]*3
+draw_board(board)
