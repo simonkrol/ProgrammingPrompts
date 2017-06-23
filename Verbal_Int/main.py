9fc526311c65d0852b7691e6cf928327da4bca1b
simonkrol <simonkrol11@gmail.com>
2017-06-22 23:04:58 -0400

    Add dictionaries and word split

--- Verbal_Int/main.py
+++ Verbal_Int/main.py
@@ -1,1 +1,8 @@
 #https://www.reddit.com/r/ProgrammingPrompts/comments/3iumvm/easymedium_write_a_program_that_translates_verbal/
+test= "One Billion, Nine hundred and Seventeen Million, Two Hundred and Two Thousand, Three Hundred and Fifty-Two";
+base_num={"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9, "ten":10}
+teen_num={"eleven":11,"twelve":12,"thirteen":13,"fourteen":14,"fifteen":15,"sixteen":16,"seventeen":17,"eighteen":18,"nineteen":19}
+ten_num={"ten":1,"twenty":2,"thirty":3,"fourty":4,"fifty":5,"sixty":6,"seventy":7,"eightty":8,"ninety":9}
+large_num={"hundred":2,"thousand":3,"million":6,"billion":9, "trillion":12}
+test=test.split()
+
