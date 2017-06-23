86824b7015777d1be3bb21a1af772c288bbcf75b
simonkrol <simonkrol11@gmail.com>
2017-06-22 23:53:19 -0400

    Add base text to number logic

--- Verbal_Int/main.py
+++ Verbal_Int/main.py
@@ -22,15 +22,49 @@ def get_length(phrase):
 		if(new_word in large_num and large_num[new_word]>cur_length):
 			cur_length=large_num[new_word]
 			cur_index=phrase.index(word)
-	if(lower_punc(phrase[0]) in ten_num):
+	if(lower_punc(phrase[0]) in ten_num or lower_punc(phrase[0])in teen_num):
 		cur_length+=2
 	elif(lower_punc(phrase[1])=="hundred" and cur_index !=1):
 		cur_length+=3
 	else:
 		cur_length+=1
+	print(cur_length)
 	return cur_length
+def to_int(text, total):
+	expected=0
+	for word in text:
+		lowered=lower_punc(word)
+		if(lowered in base_num):
+			if(expected>=1):
+				print("0"*(expected-1), end="", sep="")
+			print(base_num[lowered], end="", sep="")
+			total-=1
+			expected-=1
+		elif(lowered in teen_num):
+			if(expected>=2):
+				print("0"*(expected-2), end="", sep="")
+				total=1
+			print(teen_num[lowered], end="", sep="")
+			total-=1
+			expected=0;
+		elif(lowered in ten_num):
+			if(expected>=2):
+				print("0"*(expected-2), end="", sep="")
+				total-=e
+			print(ten_num[lowered], end="", sep="")
+			expected=1
+			total-=1
+		elif(lowered in large_num):
+			if(expected>=0):
+				print("0"*expected, end="", sep="")
+				total-=expected
+			expected=large_num[lowered]
+	if(total>0):
+		print("0"*total, end="", sep="")
+
 
 def lower_punc(text):
 	return text.translate(tbl).lower()
-
-print(get_length(input().split()))
+while(True):
+	text=input().split()
+	to_int(text,get_length(text))
